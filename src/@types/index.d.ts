87ee64c3aa6b96aa8880ff7f0bc11a3f168ead03
전솔 <jeonsol12@naver.com>
2021-07-29 15:45:06 +0900

    v1.0.2

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,2 +1,6 @@
 declare module '*.scss';
-declare module '*.svg';
+declare module '*.module.scss' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
+declare module "*.svg";
